25cae1f3fee66ba2c3a378a6967181a161dfa947
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-09-22 18:40:58 +0200

    Synchronizing methods again

--- src/ndfs/mcndfs_1_naive/Colors.java
+++ src/ndfs/mcndfs_1_naive/Colors.java
@@ -16,7 +16,7 @@ public class Colors {
   private final Map<State, Boolean> red = new HashMap<State, Boolean>();
   private int numThreads;
 
-  public Colors(int numThreads) {
+  public synchronized Colors(int numThreads) {
     this.numThreads = numThreads;
   }
   /**
@@ -29,7 +29,7 @@ public class Colors {
   *            the color
   * @return whether the specified state has the specified color.
   */
-  public boolean hasColor(State state, Color color, int threadNumber) {
+  public synchronized boolean hasColor(State state, Color color, int threadNumber) {
     Color[] current = this.color.get(state);
     if (current == null) { // initialise
       Color[] initColor = new Color[numThreads];
@@ -53,7 +53,7 @@ public class Colors {
   * @param color
   *            color to give to the state.
   */
-  public void color(State state, Color color, int threadNumber) {
+  public synchronized void color(State state, Color color, int threadNumber) {
     Color[] current = this.color.get(state);
     if (current == null) { // initialise
       Color[] initColor = new Color[numThreads];
@@ -76,7 +76,7 @@ public class Colors {
   * @param state state of which to increment the count
   * @param change amount to change the current count by
   */
-  public void changeCount(State state, int change) {
+  public synchronized void changeCount(State state, int change) {
     Integer currentCount = this.count.get(state);
     if (currentCount == null) {
       currentCount = 0;
@@ -90,7 +90,7 @@ public class Colors {
   *
   * @param state state of which to get the count
   */
-  public int getCount(State state) {
+  public synchronized int getCount(State state) {
     return this.count.get(state);
   }
 
@@ -100,7 +100,7 @@ public class Colors {
   * @param state state of which to increment the count
   * @param change amount to change the current count by
   */
-  public void setPink(State state, boolean isPink, int threadNumber) {
+  public synchronized void setPink(State state, boolean isPink, int threadNumber) {
     Boolean[] current = this.pink.get(state);
     if (current == null) {
       Boolean[] initPink = new Boolean[this.numThreads];
@@ -113,7 +113,7 @@ public class Colors {
   /**
    * Check whether the current state is pink for this thread
    */
-  public Boolean isPink(State state, int threadNumber) {
+  public synchronized Boolean isPink(State state, int threadNumber) {
     return this.pink.get(state)[threadNumber];
   }
 
@@ -122,7 +122,7 @@ public class Colors {
   *
   * @param state the state to check
   */
-  public Boolean isRed(State state) {
+  public synchronized Boolean isRed(State state) {
     Boolean current = this.red.get(state);
     if (current == null) {
       this.red.put(state, false);
@@ -130,7 +130,7 @@ public class Colors {
     return this.red.get(state);
   }
 
-  public void setRed(State state) {
+  public synchronized void setRed(State state) {
     this.red.put(state, true);
   }
 }
