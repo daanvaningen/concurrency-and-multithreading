eea37adca83da4dc20b75f575b3d5addd9a8be44
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-02 17:57:13 +0200

    Synchronized changecount

--- src/ndfs/mcndfs_2_improved/SharedData.java
+++ src/ndfs/mcndfs_2_improved/SharedData.java
@@ -20,30 +20,23 @@ public class SharedData {
   public Boolean getRed (State state) {
     Boolean s = false;
     synchronized(state){
-
       s = this.red.get(state);
-      if (s == null) s = false;
-
     }
-    
+    if (s == null) s = false;
+
     return s;
   }
 
   public void changeCount (State state, int amount) {
-    Counterlock.lock();
-    try {
-      Integer currentCount = this.count.get(state);
-      if (currentCount == null) {
-        currentCount = new Integer(amount);
-      } else {
-        currentCount = new Integer(currentCount + amount);
+      synchronized(state){
+        Integer currentCount = this.count.get(state);
+        if (currentCount == null) {
+          currentCount = new Integer(amount);
+        } else {
+          currentCount = new Integer(currentCount + amount);
+        }
+        this.count.put(state, currentCount);
       }
-      this.count.put(state, currentCount);
-    } catch (Exception e) {
-      System.out.println("exception");
-    } finally {
-      Counterlock.unlock();
-    }
   }
 
   public Integer getCount (State state) {
