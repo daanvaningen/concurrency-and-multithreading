4327c2cd28366ae8de3661ae53b412ebfa7b415d
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-03 17:43:01 +0200

    AtomicIntegers

--- src/ndfs/mcndfs_2_improved/SharedData.java
+++ src/ndfs/mcndfs_2_improved/SharedData.java
@@ -10,7 +10,7 @@ public class SharedData {
   // Shared HashMap for red states
   private volatile Map<State, Boolean> red = new HashMap<State, Boolean>();
   // Shared HashMap for the counter per state
-  private volatile Map<State, AtomicInteger> count = new HashMap<State, AtomicInteger(0)>();
+  private volatile Map<State, AtomicInteger> count = new HashMap<State, AtomicInteger>();
   // Lock for the counter
   private final ReentrantLock Counterlock = new ReentrantLock();
 
@@ -48,30 +48,7 @@ public class SharedData {
    * @param amount amount to change by (+1, -1)
    */
   public void changeCount (State state, int amount) {
-    synchronized(this.count){
-      Integer currentCount = this.count.get(state);
-      if (currentCount == null) {
-        currentCount = new Integer(amount);
-      } else {
-        currentCount = new Integer(currentCount + amount);
-      }
-      // System.out.println(currentCount);
-      this.count.put(state, currentCount);
-    }
-
-    //Counterlock.lock();
-    //try {
-      //Integer currentCount = this.count.get(state);
-      //if (currentCount == null) {
-        //currentCount = new Integer(amount);
-      //} else {
-        //currentCount = new Integer(currentCount + amount);
-      //}
-      // System.out.println(currentCount);
-      //this.count.put(state, currentCount);
-    //} finally {
-      //Counterlock.unlock();
-    //}
+    this.count.get(state).addAndAdd(amount);
   }
 
   /**
@@ -80,8 +57,7 @@ public class SharedData {
    * @param State state count to be retrieved
    */
   public Integer getCount (State state) {
-      Integer currentCount = this.count.get(state);
-      if (currentCount == null) return 0;
+      currentCount = this.count.get(state).get();
       return currentCount;
   }
 }
