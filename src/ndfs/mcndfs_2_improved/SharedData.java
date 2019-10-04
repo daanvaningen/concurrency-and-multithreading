7f2f42dda5e52ea8f7c4c546b52dd719873ffa0f
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-04 14:45:34 +0200

    Integer -> int

--- src/ndfs/mcndfs_2_improved/SharedData.java
+++ src/ndfs/mcndfs_2_improved/SharedData.java
@@ -10,7 +10,7 @@ public class SharedData {
   // Shared HashMap for red states
   private volatile Map<State, Boolean> red = new HashMap<State, Boolean>();
   // Shared HashMap for the counter per state
-  private volatile Map<State, Integer> count = new HashMap<State, Integer>();
+  private volatile Map<State, int> count = new HashMap<State, int>();
 
   /**
    * Set a the red state to true
