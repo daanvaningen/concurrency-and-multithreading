79f75123380e5b8c2f1299c6f229c95e14965372
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-06 18:09:12 +0200

    s

--- src/ndfs/mcndfs_2_improved/Worker.java
+++ src/ndfs/mcndfs_2_improved/Worker.java
@@ -67,7 +67,7 @@ public class Worker implements Callable<Void> {
     if (s.isAccepting()) {
       sharedData.changeCount(s, -1);
       while (sharedData.getCount(s) != 0 && !Thread.interrupted()) {
-        sharedData.waitUntilZero();
+        sharedData.waitUntilZero(s);
         }
     }
     colors.setPink(s, false);
