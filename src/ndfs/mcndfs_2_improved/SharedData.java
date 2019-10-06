d27d1870ab05194c15d14faabc8bec17f077813c
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-06 18:10:04 +0200

    mm

--- src/ndfs/mcndfs_2_improved/SharedData.java
+++ src/ndfs/mcndfs_2_improved/SharedData.java
@@ -82,7 +82,7 @@ public class SharedData {
     return this.count.getOrDefault(state, 0);
   }
 
-  public void waitUntilZero(State state){
+  public void waitUntilZero(State state) throws InterruptedException{
     Object Lock = this.lockmap.get(state);
     synchronized(Lock){
       Lock.wait();
