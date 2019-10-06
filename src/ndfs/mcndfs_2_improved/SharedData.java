3f73edc3f4eee6905ae07af7a21cf3299d081750
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-06 11:04:41 +0200

    private volatile

--- src/ndfs/mcndfs_2_improved/SharedData.java
+++ src/ndfs/mcndfs_2_improved/SharedData.java
@@ -18,7 +18,7 @@ public class SharedData {
   public Object SetandGetLock(State state){
     synchronized(this){
       if (this.lockmap.get(state) == null){
-        volatile Object lock = new Object();
+        private volatile Object lock = new Object();
         this.lockmap.put(state, lock);
         return lock;
       }
