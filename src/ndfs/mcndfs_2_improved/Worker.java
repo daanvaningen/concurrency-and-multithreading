9b905b8bf7e8a3560e5895bf729647335cef96a9
Sam Ferwerda <sam.ferwerda@hotmail.com>
2019-10-06 22:51:45 +0200

    reimplemented busy waitinf for the time being.

--- src/ndfs/mcndfs_2_improved/Worker.java
+++ src/ndfs/mcndfs_2_improved/Worker.java
@@ -66,11 +66,7 @@ public class Worker implements Callable<Void> {
     }
     if (s.isAccepting()) {
       sharedData.changeCount(s, -1);
-      while (sharedData.getCount(s) != 0 && !sharedData.getRed(s)) {
-        System.out.println("Zit ik hier vast?");
-        sharedData.waitUntilZero(s);
-        if (Thread.interrupted()){
-          throw new InterruptedException();
+      while (sharedData.getCount(s) != 0 && !sharedData.getRed(s) && !Thread.interrupted()) {
         }
       }
     }
